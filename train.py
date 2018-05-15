12ffc64399a6f1cc2b5c961b5748ab264a64aefc
sharf <aszarf@gmail.com>
2018-05-15 17:59:34 +0300

    tiny refactor + add data files

--- train.py
+++ train.py
@@ -1,38 +1,41 @@
 # Get command line arguments and allow for IDLE manual
 # argument input.
 import sys
-
-if sys.modules['idlelib']:
-    print("Usage: <train filename> <smoothing (Y/N)>")
-    sys.argv.extend(input("Args: ").split())
-
-filename = sys.argv[1]
-smoothing = sys.argv[2]
-
-# Read input file
-file = open(filename, 'r')
-lines = file.readlines()
-file.close()
-
-# Create parameter file
 from trainers import *
-
-filename = filename.split('.')[0]
-filename = filename + '-' + smoothing
-
-if smoothing == 'y' or smoothing == 'Y':
-    smoothing = True
-else:
-    smoothing = False
-
-Train(filename, lines, smoothing)
-
 from tree import *
 
-t = Tree()
-t.ParseFromString(lines[0])
-print(t)
-print(t.ToString())
-print(t.LeavesToString())
-print(t.CountLeaves())
-print(t.GramCount())
+def main(argv):
+	if len(argv) != 3:
+		print("Usage: <train filename> <smoothing (Y/N)>")
+		return
+
+	filename = sys.argv[1]
+	smoothing = sys.argv[2]
+
+	# Read input file
+	file = open(filename, 'r')
+	lines = file.readlines()
+	file.close()
+
+	# Create parameter file
+	filename = filename.split('.')[0]
+	filename = filename + '-' + smoothing
+
+	if smoothing == 'y' or smoothing == 'Y':
+		smoothing = True
+	else:
+		smoothing = False
+
+	Train(filename, lines, smoothing)
+
+	t = Tree()
+	t.ParseFromString(lines[0])
+	print(t)
+	print(t.ToString())
+	print(t.LeavesToString())
+	print(t.CountLeaves())
+	print(t.GramCount())
+
+
+if __name__ == '__main__':
+	main(sys.argv)
