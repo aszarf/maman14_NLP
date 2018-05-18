99c9d44e103d50ae2d1da3646edeee8764266950
sharf <aszarf@gmail.com>
2018-05-18 13:15:06 +0300

    generate lex and gram files

--- trainers.py
+++ trainers.py
@@ -1,6 +1,7 @@
 # Main training module
 
 from tree import *
+import math
 
 def Train(filename, lines, smoothing):
     
@@ -14,16 +15,28 @@ def Train(filename, lines, smoothing):
         
         for key in leaves_dict.keys():
             if not key in leaves_count.keys():
-                leaves_count[key] = 0
+                leaves_count[key] = leaves_dict[key]
             leaves_count[key] = leaves_count[key] + leaves_dict[key]
                 
         for key in gram_dict.keys():
             if not key in gram_count.keys():
-                gram_count[key] = 0
+                gram_count[key] = gram_dict[key]
             gram_count[key] = gram_count[key] + gram_dict[key]
-
-    # TODO: calculate logprobs
-    # TODO: write lex file 
-    # TODO: write gram file
     
-    return
+    leaves_sum = float(reduce(lambda x,y: x+y, leaves_count.values()))
+    buf = ''
+    for key in leaves_count:
+        prob = -math.log(leaves_count[key]/leaves_sum)
+        line = "%s\t%s\t%f\n" % (key[1], key[0], prob)
+        buf = buf + line
+    open(filename + ".lex", "wb").write(buf)
+    
+    gram_sum = float(reduce(lambda x,y: x+y, gram_count.values()))
+    buf = ''
+    for key in gram_count:
+        prob = -math.log(gram_count[key]/gram_sum)
+        line = "%f\t%s\t%s\n" % (prob, key[0], ' '.join(key[1]))
+        buf = buf + line
+    open(filename + ".gram", "wb").write(buf)
+        
+    return True
