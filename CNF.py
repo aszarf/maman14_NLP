ae5d733ea1e11e053739c6e930981bcac403bc44
sharf <aszarf@gmail.com>
2018-06-03 01:25:15 +0300

    minor refactor in CNF

--- CNF.py
+++ CNF.py
@@ -30,18 +30,18 @@ def move_asterisk_right(label):
 def children_to_tuples(children_list,wanted_parent,h):
     new_child = Tree.Node()
     new_child.data.label = move_asterisk_right(wanted_parent.data.label)
-    new_child.children = []
     
     if len(children_list) <= 2:
         new_child.children = children_list
+        new_child.num_children = len(new_child.children)
     else:
         left_child = Markovize(children_list[0], h)
         right_child = Markovize(children_to_tuples(children_list[1:],new_child, h), h)
         new_child.children = [left_child, right_child]
+        new_child.num_children = 2
         for c in new_child.children:
             ApplyCNF(c, h)
-    new_child.num_children = len(new_child.children)
-
+    
     return new_child
     
 # Transforms grammar tree into CNF grammar tree
@@ -56,17 +56,15 @@ def ApplyCNF(t, h):
     
     if len(n.children) > 2:
         n.data.label = "%s*%s" % (n.data.label, "-".join(map(lambda x: x.data.label.split(' ')[0], n.children)))
-        children = n.children
-        n.children = []
         
-        left_child = Markovize(children[0], h)
-        right_child = Markovize(children_to_tuples(children[1:],children[1].parent, h), h)
+        left_child = Markovize(n.children[0], h)
+        right_child = Markovize(children_to_tuples(n.children[1:],n.children[1].parent, h), h)
         n.children = [left_child, right_child]
         n.num_children = 2
     for c in n.children:
         ApplyCNF(c, h)
         
-    return t
+    return n
 
 # Transforms CNF grammar tree into original grammar tree
 def RemoveCNF(t):
@@ -78,7 +76,7 @@ def RemoveCNF(t):
     else:
         n = t
           
-    t = DeMarkovize(t)
+    n = DeMarkovize(n)
             
     for c in n.children:
         out = RemoveCNF(c)
@@ -96,5 +94,5 @@ def RemoveCNF(t):
         if len(n.data.label.split('*')[0].split('-')) != 1:
             return n.children
         
-    return t
+    return n
 
